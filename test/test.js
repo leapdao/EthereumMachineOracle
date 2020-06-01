6a898173932d0a3be2d143a6b02c247645ff60d4
Jan <jan@parseclabs.org>
2020-06-01 15:22:13 +0200

    Try apply proxy.

--- test/test.js
+++ test/test.js
@@ -15,6 +15,25 @@ describe('EMO', () => {
   it('test1', async () => {
     const [machine, merkle, oracle, court] = await loadFixture(fixture);
     const question = await oracle.getAnswer(ethers.constants.HashZero);
-    console.log(question);
+    console.log(oracle.interface._abiCoder);
+    
+    const oldGetCoder = oracle.interface._abiCoder._getCoder;
+    const handler = {
+      apply: function(target, thisArg, argumentsList) {
+        console.log(argumentsList);
+        return target(...argumentsList);
+      }
+    };
+    const newGetCoder = new Proxy(oldGetCoder, handler);
+    oracle.interface._abiCoder._getCoder = newGetCoder;
+
+    const askTx = await oracle.ask(
+      {
+        temp: ["0x0000000000000000000000000000000000000000000000000000000000000000"]
+      },
+      5,
+      "0x333333333333333333333333333333333333333333333333",
+      "0x333333333333333333333333333333333333333333333333"
+    );
   });
 });
