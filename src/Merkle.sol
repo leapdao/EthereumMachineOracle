3216d2ed5f2bfaeb6f78e0da7311b589f80d0ed6
Kosta Korenkov <kosta@korenkov.net>
2020-08-26 13:00:02 +0300

    feat: implement Merkle.eval

--- src/Merkle.sol
+++ src/Merkle.sol
@@ -10,7 +10,9 @@ library Merkle {
   }
 
   struct Proof {
+    bytes32 leaf;
     bytes32[] data;
+    uint8[] path;
   }
 
   function hash (TreeNode memory self)
@@ -26,7 +28,22 @@ library Merkle {
     pure
     returns (bytes32 leaf, bytes32 root, uint index)
   {
-    
+    require(self.data.length == self.path.length, "data and path should be of the same size");
+
+    leaf = self.leaf;
+    root = leaf;
+    index = 0;
+
+    for (uint256 i = 0; i < self.data.length; i++) {
+        bytes32 proofElement = self.data[i];
+        index = (index << 1) | (self.path[self.data.length - i - 1] > 0 ? 0 : 1);
+
+        if (self.path[i] > 0) {
+            root = keccak256(abi.encodePacked(root, proofElement));
+        } else {
+            root = keccak256(abi.encodePacked(proofElement, root));
+        }
+    }
   }
   
 }
