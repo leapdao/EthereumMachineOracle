735d53a507d5b4b218b22c9dbb8e9a1623039211
roleengineer <roleengineer@gmail.com>
2020-09-17 08:28:40 +0300

    add analyzer

--- echidna-docker/machine_approver/run.sh
+++ echidna-docker/machine_approver/run.sh
@@ -6,6 +6,8 @@ if [[ $result = "Your file passed basic checker." ]]
  then
    echo "Echidna starts fuzzing, please wait."
    cd ./echidna
-   echidna-test Test.sol --contract Test --config config.yaml
+   echidna_res=$(echidna-test Test.sol --contract Test --config config.yaml)
    echo "The fuzzing finished."
+   analized=$(node ../analyzer.js $echidna_res)
+   echo "$analized"
 fi
